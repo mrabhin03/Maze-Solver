1d83da08ac42d43ab77d26b04dffab09f1525d74
Abhin M <130139390+mrabhin03@users.noreply.github.com>
2024-11-20 07:44:24 +0530

    Small Changes

--- Script.js
+++ Script.js
@@ -4,6 +4,7 @@ const max = 50;
  rownum = max; 
  colnum = max;   
 reset = true;
+alert(window.innerWidth)
 
 function inputInsert() {
     const grid = Array.from({ length: rownum }, () => Array(colnum).fill('wall'));
